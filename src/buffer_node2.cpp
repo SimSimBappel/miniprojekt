9dec89b52578f0a48bca7ed527526ac2016d5b97
lass6230 <due6230@gmail.com>
2020-12-27 11:14:28 +0100

    service

--- src/buffer_node2.cpp
+++ src/buffer_node2.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include "ros/ros.h"
-//#include <my_code_msgs/myString.h>
+#include <my_code_msgs/myString.h>
+#include <my_code_msgs/myStringRequest.h>
+#include <my_code_msgs/myStringResponse.h>
 
+using namespace std;
 
+bool callBack(my_code_msgs::myStringRequest &req, my_code_msgs::myStringResponse &res){
+    cout << req.str << endl;
+    string response = "hej";
+    res.str = response;
+    return true;
+}
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "buffer_node2");
+    ros::NodeHandle nh;
+    ros::ServiceServer service = nh.advertiseService("service_talker",callBack);
+    ros::spin();
     return 0;
+
 }
--- src/buffer_node_tester.cpp
+++ src/buffer_node_tester.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 #include "ros/ros.h"
-//#include <my_code_msgs/myString.h>
+#include <cstdlib>
+#include <string>
 
+#include <my_code_msgs/myString.h>
+#include <my_code_msgs/myStringRequest.h>
+#include <my_code_msgs/myStringResponse.h>
+using namespace std;
 
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "buffer_node_tester");
+    ros::NodeHandle nh;
+    ros::ServiceClient client = nh.serviceClient<my_code_msgs::myString>("service_talker");
+    
+    my_code_msgs::myString srv;
+
+    string test = "plads: 11,seng: 6,ja,-1.03911781216,11.1295263925,0.753757344949,0.0";
+    srv.request.str = test;
+    //cout << srv << endl;
+    if(client.call(srv)){
+        cout << "calling :)" << endl;
+        cout << "anwser: " << srv.response << endl;
+    }
+    else{
+        cout << "do not work" << endl;
+    }
+
     return 0;
 }
