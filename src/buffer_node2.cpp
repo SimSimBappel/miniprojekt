b5a46937bf581c920bab08fed8d770d20cf64db6
lass6230 <due6230@gmail.com>
2020-12-27 10:14:34 +0100

    buffer_node2

---
+++ src/buffer_node2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "ros/ros.h"
+// #include <my_code_msgs/myString.h>
+
+
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "buffer_node");
+    return 0;
+}
