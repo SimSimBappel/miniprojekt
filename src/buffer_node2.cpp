ca2505078041e688100f52366a6e82072b0364bc
lass6230 <due6230@gmail.com>
2020-12-28 19:48:24 +0100

    sort

--- src/buffer_node2.cpp
+++ src/buffer_node2.cpp
@@ -89,53 +89,45 @@ void terminalWrite(){
 }
 
 void sortering(){
-    cout << "1" << endl;
-int good = 0;
-while(good != liste.he.size()){
+    cout << "sorting" << endl;
+int good = 1;
+while(good == 1){
     good = 0;
 
     for(int i = 0; i < liste.he.size(); i++){
       if(i+1 < liste.he.size()) { 
         
-        cout << "sorting" << endl;
         string holder[4];
-        cout << "2" << endl;
+        
         stringstream s;
         stringstream ss;
         ss << liste.he[i+1];
-        cout << "3" << endl;
+        
         s << liste.he[i];
-        cout << "4" << endl;
+        
         getline(s, holder[0], ' ');
-        cout << "5" << endl;
+        
         getline(s, holder[1], ',');
-        cout << "6" << endl;
+        
         getline(ss, holder[2], ' ');
-        cout << "7" << endl;
+        
         getline(ss, holder[3], ',');
-        cout << "8" << endl;
+        
         int check_1 = stoi(holder[1]);
-        cout << "9" << endl;
+        
         int check_2 = stoi(holder[3]);
-        cout << "10" << endl;
-        if(check_1 < check_2 && i+1 < liste.he.size() ){
-            cout << "swap" << endl;
-            swap(liste.he[i],liste.he[i+1]);
-            terminalWrite();
+        
+        if(check_1 > check_2 && i+1 < liste.he.size() ){
             
+            swap(liste.he[i],liste.he[i+1]);
+            
+            good = 1;
         }
-        else{
-            good ++;
-        }
+        
       }
 
     }
-        // getline(, holder[1], ',');
-        
-        
-
-    }
     
- 
+    }
 
 }
