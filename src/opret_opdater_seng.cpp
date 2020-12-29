789ca1e91bfdf54913f36fd57946d5099c329414
SimSimBappel <simontk2010@hotmail.com>
2020-12-29 11:15:34 +0100

    den virker bedre

--- src/opret_opdater_seng.cpp
+++ src/opret_opdater_seng.cpp
@@ -45,114 +45,112 @@ int main(int argc, char **argv)
     bool done = false;
     string input;
     string opgave;
-    cout << "hej"<< endl;
     
-    //while(ok){
+    while(ok)
+    {
 
 
-    if(done == true){
-       cout << "Vil du 1: tjekke, 2: oprette, 3: opdatere en plads eller 4: push til fil? \n";
-   }
-   else{
-        cout << "Vil du 1: tjekke, 2: oprette eller 3: opdatere en plads? \n";
-   
-   }
-    cin >> opgave;
+        if(done == true){
+        cout << "Vil du 1: tjekke, 2: oprette, 3: opdatere en plads eller 4: push til fil? \n";
+        }
+        else{
+            cout << "Vil du 1: tjekke, 2: oprette eller 3: opdatere en plads? \n";
     
-    if (opgave == "tjekke" || opgave == "1"){
-        job = 1;
-    } 
-    else if (opgave == "oprette" || opgave == "2"){
-        job = 2;
-    }
-    else if (opgave == "opdatere" || opgave == "3"){
-        job = 3;
-    }
-    else if (opgave == "push" || opgave == "4"){
-        job = 4;
-    }
-
-    string line;
-    fstream file;
-    NodeHandle nh;
-    ServiceClient client = nh.serviceClient<miniprojekt::newString>("service_talker" );
-    miniprojekt::newString srv;
-
-    switch (job){
-
-        case(1):
-           {    
-            //tjekke
-            file.open(getFilename());
-            
-            cout << "Vælg en plads:";
-            cin >> input;
-            
-            int lineno = stoi(input);
-            int lines_read = 0;
-
-            if (file.is_open()){
-
-                while(file){
-            
-                    getline(file,line);
-                    lines_read++;
-            
-
-            if(lines_read==lineno){
-                line.resize(16);
-                cout << line << endl;
-                break;
-                    }   
-                }
-        file.close();
-        if (lines_read<lineno)
-            cout << "Kunne ikke finde plads" << lineno << '\n';
-            return 0;
-            
-            }
-            break;
         }
 
+        cin >> opgave;
+        
+        if (opgave == "tjekke" || opgave == "1"){
+            job = 1;
+        } 
+        else if (opgave == "oprette" || opgave == "2"){
+            job = 2;
+        }
+        else if (opgave == "opdatere" || opgave == "3"){
+            job = 3;
+        }
+        else if (opgave == "push" || opgave == "4"){
+            job = 4;
+        }
 
+        string line;
+        fstream file;
+        NodeHandle nh;
+        ServiceClient client = nh.serviceClient<miniprojekt::newString>("service_talker" );
+        miniprojekt::newString srv;
 
-        case(2):
-        {
-            //oprette
-            // init(argc, argv, "class_forsog_node");
-            
-            int plads;
-            int seng;
+        switch (job){
 
-            
-            // file.open(getFilename(),ios::app);
+            case(1):
+            {    
+                //tjekke
+                file.open(getFilename());
+                
+                cout << "Vælg en plads:";
+                cin >> input;
+                
+                int lineno = stoi(input);
+                int lines_read = 0;
 
-            cout << "Venligst skriv plads nummer \n" ;
-            cin >> plads;
+                if (file.is_open()){
 
-            cout << "Skriv senge nummer eller 0 for ingen seng \n";
-            cin >> seng;
-            string bed;
-            if(seng== 0){
-                bed = "nej";
+                    while(file){
+                
+                        getline(file,line);
+                        lines_read++;
+                
+
+                if(lines_read==lineno){
+                    line.resize(16);
+                    cout << line << endl;
+                    break;
+                        }   
+                    }
+            file.close();
+            if (lines_read<lineno)
+                cout << "Kunne ikke finde plads" << lineno << '\n';
+                return 0;
+                
+                }
+                break;
             }
-            else{
-                bed = "ja";
-            }
 
-            
 
-            Subscriber sub = nh.subscribe("amcl_pose", 1000, counterCallback);
-            
-            // Sætter hvor mange gange den skal lave callbacket hvert sekund
-            Rate loop_rate(10);
 
-            for(int i = 0; i<10; i++){
-                spinOnce();
-                loop_rate.sleep();
-            }
-            
-            stringstream ff;
+            case(2):
+            {
+                //oprette
+                
+                int plads;
+                int seng;
+
+
+                cout << "Venligst skriv plads nummer \n" ;
+                cin >> plads;
+
+                cout << "Skriv senge nummer eller 0 for ingen seng \n";
+                cin >> seng;
+                string bed;
+                if(seng== 0){
+                    bed = "nej";
+                }
+                else{
+                    bed = "ja";
+                }
+
+                
+
+                Subscriber sub = nh.subscribe("amcl_pose", 1000, counterCallback);
+                
+                // Sætter hvor mange gange den skal lave callbacket hvert sekund
+                Rate loop_rate(10);
+
+                for(int i = 0; i<10; i++){
+                    spinOnce();
+                    loop_rate.sleep();
+                }
+                
+                stringstream ff;
 
                 ff << "plads: " << plads << ",seng: " << seng << "," << bed << "," <<info.x << ","<< info.y << "," << info.z << "," << info.w;
                 string ss = ff.str();
@@ -163,136 +161,143 @@ int main(int argc, char **argv)
                 srv.request.str = ss;
                 
                 if(client.call(srv)){
-                        cout << "calling :)" << endl;
-                        cout << "anwser: " << srv.response << endl;
-                        done = true;
-                    }
-                    else{
-                            cout << "do not work" << endl;
+                    cout << "calling :)" << endl;
+                    cout << "anwser: " << srv.response << endl;
+                    done = true;
+                }
+                else{
+                    cout << "does not work" << endl;
+                }
+
+
+                break;
+            }
+
+            case(3):
+            {
+                //opdatere
+                file.open(getFilename(), ios::in | ios::out);
+                cout << "Vælg en plads du vil opdatere: ";
+                cin >> input;
+                int line_to_change = stoi(input);
+                
+                string seng;
+                cout << "angiv ny sengenummer" << '\n';
+                cin >> seng;
+                int bed_nr = stoi(seng);
+                if(bed_nr > 999)
+                {
+                    exit(1);
+                }
+
+                string tokens[checkLineNr()+1][7];
+                int line = 1; 
+                unsigned int file_pos = 0;
+                if(file.is_open()){
+                    string line_elements[7] = "";
+                    
+                    while(getline(file, line_elements[0], ',')){
+                        tokens[line][0] = line_elements[0];
+
+                        for (int i = 1; i < 6; i++)
+                        {
+                            getline(file, line_elements[i], ',');
+                            tokens[line][i] = line_elements[i];
                         }
-//den er streget ud da vi nok nu kan køre det gennem buffer_node2
-            // if (file.is_open()){
-            //     file << "plads: " << plads << ",seng: " << seng << "," << bed << "," <<info.x << ","<< info.y << "," << info.z << "," << info.w;
-                
-                
-            // }
-        
+                        getline(file, line_elements[6]);
+                        tokens[line][6] = line_elements[6];
+                        
 
-            // file.close();
+                        line ++;
+                        if(line == line_to_change){ 
+                            file_pos = file.tellg();
 
-            break;
-        }
-
-
-
-        case(3):
-        {
-            //opdatere
-            file.open(getFilename(), ios::in | ios::out);
-            cout << "Vælg en plads du vil opdatere: ";
-            cin >> input;
-            int line_to_change = stoi(input);
-            
-            string seng;
-            cout << "angiv ny sengenummer" << '\n';
-            cin >> seng;
-            int bed_nr = stoi(seng);
-            cout << bed_nr << endl;
-
-            string tokens[checkLineNr()+1][7];
-            int line = 1; 
-            unsigned int file_pos = 0;
-            if(file.is_open()){
-                string line_elements[7] = "";
-                
-                while(getline(file, line_elements[0], ',')){
-                    tokens[line][0] = line_elements[0];
-                    //cout << "line: " << line <<'\n';//delete
-                    cout << tokens[line][0] << '\n';//delete
-                    //file.seekg(0,ios::beg);
-                    for (int i = 1; i < 6; i++)
-                    {
-                        getline(file, line_elements[i], ',');
-                        tokens[line][i] = line_elements[i];
-                        cout << tokens[line][i] << '\n'; // delete
-                    }
-                    getline(file, line_elements[6]);
-                    tokens[line][6] = line_elements[6];
-                    cout << tokens[line][6] << '\n' << '\n';// delete
-
-                    line ++;
-                    if(line == line_to_change){ // find den rigtige plads til denne for at den kan tage fra linje 1 og til slut
-                        file_pos = file.tellg();
-                        cout << "file.tellg: " << file.tellg() << endl;
-
-                        cout << "file_pos: " <<file_pos << endl;
+                            cout << "file_pos: " <<file_pos << endl;
+                        }
+                        
                     }
                     
+
+                    
+
                 }
-                
+                file.close();
 
-                //succing edit time
+                file.open(getFilename(), ios:: out | ios::in);
 
+                if(file.is_open())
+                {
+                    
+                    //cout << file_pos << endl;
+                    file.seekp(file_pos);
+                    string jn = "";
+                    if(bed_nr == 0)
+                    {
+                        jn= "nej";
+                    }
+                    else{
+                        jn = "ja";
+                    }
+
+                    file << tokens[line_to_change][0] << ",seng: " << bed_nr << "," << jn;
+                    
+                    // if (!tokens[line_to_change][6].empty() && tokens[line_to_change][6][tokens[line_to_change][6].length()-1] == '\n') {
+                    //     tokens[line_to_change][6].erase(tokens[line_to_change][6].length()-1);
+                    // }
+
+                    for(int i = 3; i < 6; i++)
+                    {
+                        file << "," << tokens[line_to_change][i];
+                    }
+                    file << ",0.0  ";
+
+                    
+                    
+                    cout << "token line_to_change: "<< tokens[line_to_change][0] << endl;
+                    string temper ="";
+                    file.seekg(file_pos);
+                    getline(file, temper);
+                    cout << "temper: " << temper << endl;
+                }
+                break;
             }
-            file.close();
-
-            file.open(getFilename(), ios:: out | ios::in);
-
-            if(file.is_open()){
-                
-                //cout << file_pos << endl;
-                file.seekp(file_pos);
-
-                file << tokens[line_to_change][0] << ",seng: " << bed_nr <<",";
-                //file << tokens[line_to_change][0];
-                cout << "token line_to_change: "<< tokens[line_to_change][0] << endl;
-                string temper ="";
-                file.seekg(file_pos);
-                getline(file, temper);
-                cout << "temper: " << temper << endl;
-
-
+            case(4):
+            {
+                srv.request.head = "push";
+                srv.request.str = "";
+                if(client.call(srv)){
+                    cout << "calling :)" << endl;
+                    cout << "anwser: " << srv.response << endl;
+                }
+                else{
+                    cout << "do not work" << endl;
+                }
+                break;
             }
-            break;
-        }
-        case(4):
-        {
-            srv.request.head = "push";
-            srv.request.str = "";
-            if(client.call(srv)){
-                cout << "calling :)" << endl;
-                cout << "anwser: " << srv.response << endl;
+            default:
+            {
+                cout << "Something went wrong" << endl;
             }
-            else{
-                cout << "do not work" << endl;
-            }
-            break;
-        }
-        default:
-        {
-            cout << "Wrong" << endl;
+
         }
 
     }
-
-   // }
 }
 
 int checkLineNr(){
   fstream newfile;
   int doc_line_nr = 0;
-  
+  string temp ="";
   newfile.open(getFilename(),ios::in);
   if (newfile.is_open())
   {
-    string temp;
     while(getline(newfile, temp)){
       doc_line_nr++;
-      //cout << temp << '\n'; //delete this
+      
     }
   }
   newfile.close();
-  //cout << doc_line_nr << '\n';
+  
   return  doc_line_nr;
 }
 
